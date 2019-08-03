5ebec692d20b6f2dbee9a4500c5813fa80b4cb62
panjiahui1214 <1300135503@qq.com>
2019-08-03 23:40:44 +0800

    almost finish admin view

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,39 +1,21 @@
 (function () {
     aside_display_or_not();
-    aside_menu_active();
 
     /**
      * 根据点击按钮控制是否显示侧边栏
      */
     function aside_display_or_not () {
-        var btn_nav = document.getElementById("btn-nav");
+        var btn_nav = document.getElementsByClassName("nav_btn")[0];
         btn_nav.onclick = function () {
-            var aside = document.getElementsByTagName("aside")[0];
-            var content = document.getElementById("content");
+            var aside = document.getElementsByClassName("aside")[0];
+            var content = document.getElementsByClassName("main_content_wrapper")[0];
             if (!aside.style.display) {
                 aside.style.display = "none";
-                content.classList.remove("l-200px");
-                content.classList.add("l-0");
+                content.style.left = "0";
             }
             else {
                 aside.style.display = "";
-                content.classList.remove("l-0");
-                content.classList.add("l-200px");
-            }
-        }
-    }
-    //
-    /**
-     * 侧边栏高亮当前菜单
-     */
-    function aside_menu_active() {
-        var aside_menu = document.getElementById("aside_menu");
-        for (var i = 0; i < aside_menu.children.length; i ++) {
-            aside_menu.children[i].onclick = function () {
-                for (var j = 0; j < aside_menu.children.length; j ++) {
-                    aside_menu.children[j].classList.remove("active");
-                }
-                this.classList.add("active");
+                content.style.left = "200px";
             }
         }
     }
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -9,47 +9,56 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
 </head>
-<body class="h-100">
-    <header class=" h-50px bg-primary">
-        <div class="w-200px text-center big-font text-white h-lh-50px float-left">皮卡丘潘的博客</div>
-        <button id="btn-nav" class="btn-info-no-border h-50px p-0 w-50px navbar-light cursor-pointer">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-    </header>
-    <aside class="w-200px position-fixed t-50px l-0 h-100 shadow-lg">
-        <ul class="list-unstyled text-center mt-3">
-            <li>admin</li>
-            <li>超级管理员</li>
-        </ul>
-        <div id="aside_menu" class="list-group text-center bg-white">
-            <a href="#" class="list-group-item active">会员管理</a>
-            <a href="#" class="list-group-item">文章管理</a>
-            <a href="#" class="list-group-item">系统管理</a>
+<body>
+
+    <div class="wrapper">
+        <div class="nav">
+            <div class="nav_icon">皮卡丘潘的博客</div>
+            <button class="nav_btn navbar-light">
+                <span class="navbar-toggler-icon"></span>
+            </button>
         </div>
-    </aside>
-    <div id="content" class="position-fixed h-100 w-100 l-200px">
-        <div class="m-l-30px h-lh-30px">
-            <span class="p-l-15px border-3-left text-primary">会员管理</span>
-        </div>
-        <div class="m-l-30px h-100 w-100 bg-white shadow-lg border-all">
-            <div>
-                <table class="table table-hover">
-                    <tr>
-                        <th>ID</th>
-                        <th>昵称</th>
-                        <th>手机号</th>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>panjiahui</td>
-                        <td>13012345678</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>panjiahui2</td>
-                        <td>13012345679</td>
-                    </tr>
-                </table>
+        <div class="content">
+            <div class="aside shadow-lg">
+                <div class="aside_admin_wrapper">
+                    <ul class="list-unstyled">
+                        <li>admin</li>
+                        <li>超级管理员</li>
+                    </ul>
+                </div>
+                <div class="aside_menu_wrapper">
+                    <ul class="list-unstyled">
+                        <a href=""><li class="active">会员管理</li></a>
+                        <a href=""><li>文章管理</li></a>
+                        <a href=""><li>系统管理</li></a>
+                    </ul>
+                </div>
+            </div>
+            <div class="main_content_wrapper">
+                <div class="main_content_menu">
+                    <span>会员管理</span>
+                </div>
+                <div class="main_content shadow-lg">
+                    <div class="table_wrapper">
+                        <table class="table table-hover">
+                            <tr class="th">
+                                <th>ID</th>
+                                <th>用户名</th>
+                                <th>手机号码</th>
+                                </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>panjiahui</td>
+                                <td>13012345678</td>
+                                </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>panjiahui2</td>
+                                <td>13012345679</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
